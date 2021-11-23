908d67bbeba38145fdfaf77667b798f0c7910e2c
Bruno Olympio <b.olympio@reply.com>
2021-11-23 09:13:54 -0300

    Projeto final do Curso

---
+++ Blog/Program.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models;
+using Blog.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new BlogDataContext();
+        }
+    }
+}
